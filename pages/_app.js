a00936758608fff9385f4d25e474d9d856dbf92d
coachkim <kimsw0825@gmail.com>
2023-02-01 21:26:50 +0900

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import '@/styles/globals.css'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const goToHome = () => {
+    router.push("/");
+  }
+  const goToAbout = () => {
+    router.push("/about")
+  }
+  return (
+    <div>
+      <h1>Header</h1>
+      <div onClick={goToHome}>Home</div>
+      <div onClick={goToAbout}>about</div>
+      <Component {...pageProps} />
+      <h1>Footer</h1>
+    </div>
+  )
 }
